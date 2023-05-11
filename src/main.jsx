9d2e7a39708174afeac6d61d13671e320df1cb6a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-12 00:20:27 +0300

    updated jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 import './index.css'
-import {Provider} from './themeConext.js'
+import {NameContextProvider} from './themeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider value={'Joshua Ombasa'}>
+    <NameContextProvider>
       <App />
-    </Provider>
+    </NameContextProvider>
   </React.StrictMode>,
 )
